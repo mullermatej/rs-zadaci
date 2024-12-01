ee244d564ad82273512aec00a6409e2d9dca2046
Matej <mmuller@student.unipu.hr>
2024-12-01 18:46:17 +0100

    Riješen zadatak 1

---
+++ RS-5-zadaće/zadatak_1.py
@@ -0,0 +1,16 @@
+import asyncio
+import aiohttp
+from aiohttp import web
+
+app = web.Application()
+
+
+async def dohvati_proizvode(request):
+    proizvodi = [{"naziv": "banana", "kolicina": 5, "cijena": 2},
+                 {"naziv": "jabuka", "kolicina": 3, "cijena": 1}]
+    return web.json_response(proizvodi)
+
+
+app.router.add_get("/", dohvati_proizvode)
+
+web.run_app(app, host="localhost", port=8081)
